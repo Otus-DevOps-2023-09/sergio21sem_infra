8e65547f0ecc793b64103f10997ae4d0308e972d
Sergei Semenov <s.semenov@selectel.org>
2023-11-10 17:34:43 +0300

    use terraform modules

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,11 @@ variable "bucket_name" {
 variable "name" {
   description = "SA User Name"
 }
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "fd80hkpns99fn47ol9cf"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "fd8f82bhflu4e2igk2aq"
+}
