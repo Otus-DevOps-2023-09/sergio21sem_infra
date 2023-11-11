528ef7799fc33764765ee23b595961988264b805
Sergei Semenov <s.semenov@selectel.org>
2023-11-11 20:41:00 +0300

    create terraform tfvars.expample on env

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -39,3 +39,12 @@ variable "db_disk_image" {
   description = "Disk image for reddit db"
   default     = "fd8f82bhflu4e2igk2aq"
 }
+variable "service_account_key_file" {
+  description = "Service account key file"
+}
+variable "bucket_name" {
+  description = "Name of S3 bucket"
+}
+variable "name" {
+  description = "SA User Name"
+}
